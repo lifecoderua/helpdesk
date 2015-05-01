a8d1af2890e239f6a9fe39e010cd26fcb36cd5fa
Alexei Volkov <lifecoder.ua@gmail.com>
2015-05-01 23:19:43 +0300

    gems and db configuration

---
+++ app/models/staff.rb
@@ -0,0 +1,6 @@
+class Staff < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  # , :registerable, :validatable
+  devise :database_authenticatable, :recoverable, :rememberable, :trackable
+end
