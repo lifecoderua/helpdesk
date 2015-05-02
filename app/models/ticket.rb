aeb26f6cdb50e41dd2d779892a637c05a104712f
Alexei Volkov <lifecoder.ua@gmail.com>
2015-05-02 17:24:01 +0300

    tickets scaffold added

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,4 +1,14 @@
 class Ticket < ActiveRecord::Base
   belongs_to :department
   belongs_to :owner, class: 'Staff'
+
+  after_save :generate_slug
+
+private
+
+  def generate_slug
+    if self.id_changed? && !self.slug_changed?
+      self.update_attribute(:slug, self.id)
+    end
+  end
 end
