32c7fd34f3b162fa018ab16118d096d1da4218ae
Alexei Volkov <lifecoder.ua@gmail.com>
2015-05-02 21:09:55 +0300

    one more tiny test

--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -2,6 +2,12 @@ require 'rails_helper'
 
 RSpec.describe Ticket, type: :model do
 
+  it 'creates ticket with specific ID required for slug test' do
+    id = 290
+    ticket = FactoryGirl.build(:ticket, id: id)
+    expect(ticket.id).to eq(id)
+  end
+
   it 'generates semi-random slug' do
     ticket = FactoryGirl.build(:ticket)
     expect(ticket.slug).to eq(ticket.id)
