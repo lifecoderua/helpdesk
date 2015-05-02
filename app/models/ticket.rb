abdf228f10764485277e81f5d201b2608508e8e1
Alexei Volkov <lifecoder.ua@gmail.com>
2015-05-02 22:08:25 +0300

    slug generation test

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -4,11 +4,20 @@ class Ticket < ActiveRecord::Base
 
   after_save :generate_slug
 
+  # Strict limit which means id's may become not unique.
+  # If the system pushed it - slug generation must be revised
+  # (with the system in general, which should be obsolete at the point of 15.6*10^9 tickets)
+  # * You should never push it with the regular DB settings, which provides int32 for id column
+  # it is HH-AAA-HH
+  TICKET_ID_LIMIT = 15619063808
+
 private
 
   def generate_slug
     if self.id_changed? && !self.slug_changed?
-      self.update_attribute(:slug, self.id)
+      slug = Rails.application.secrets.ticket[:shift]
+
+      self.update_attribute(:slug, slug)
     end
   end
 end
--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -9,8 +9,21 @@ RSpec.describe Ticket, type: :model do
   end
 
   it 'generates semi-random slug' do
-    ticket = FactoryGirl.build(:ticket)
-    expect(ticket.slug).to eq(ticket.id)
+    id = 1
+    slug = Rails.application.secrets.ticket[:test_plus_one]
+
+    ticket = FactoryGirl.build(:ticket, id: id)
+    ticket.save
+    expect(ticket.slug).to eq(slug)
   end
+
+  # The ticket limit is quite big for regular DB id column.
+  # This test may become handly for custom setups, but the default int32 catches RangeError way earlier
+  # it 'slug exception raised on limit overflow' do
+  #   id = Ticket::TICKET_ID_LIMIT
+  #
+  #   ticket = FactoryGirl.build(:ticket, id: id)
+  #   expect{ticket.save}.to raise_error(Exception, 'strict limit pushed')
+  # end
 
 end
