d928bb7649fd10c549759422afed566c9acca580
Alexei Volkov <lifecoder.ua@gmail.com>
2015-05-02 23:09:36 +0300

    ticket validation tests

--- spec/factories/tickets.rb
+++ spec/factories/tickets.rb
@@ -2,6 +2,8 @@ FactoryGirl.define do
   factory :ticket do
     subject 'Test Ticket'
     body 'I want to take some help on my very special server issue NOW!!!!'
+    customer_name 'Customer One'
+    email 'customer+one@example.com'
   end
 
 end
--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -17,6 +17,36 @@ RSpec.describe Ticket, type: :model do
     expect(ticket.slug.slice(4, 9)).to eq(slug)
   end
 
+  it 'creates valid ticket' do
+    ticket = FactoryGirl.build(:ticket)
+    expect(ticket).to be_valid
+  end
+
+  it 'validates name presence' do
+    ticket = FactoryGirl.build(:ticket, customer_name: '')
+    expect(ticket).to_not be_valid
+  end
+
+  it 'validates email presence' do
+    ticket = FactoryGirl.build(:ticket, email: '')
+    expect(ticket).to_not be_valid
+  end
+
+  it 'validates email is correct' do
+    ticket = FactoryGirl.build(:ticket, email: 'randomstringhere@is')
+    expect(ticket).to_not be_valid
+  end
+
+  it 'validates subject presence' do
+    ticket = FactoryGirl.build(:ticket, subject: '')
+    expect(ticket).to_not be_valid
+  end
+
+  it 'validates body presence' do
+    ticket = FactoryGirl.build(:ticket, body: '')
+    expect(ticket).to_not be_valid
+  end
+
   # The ticket limit is quite big for regular DB id column.
   # This test may become handly for custom setups, but the default int32 catches RangeError way earlier
   # it 'slug exception raised on limit overflow' do
