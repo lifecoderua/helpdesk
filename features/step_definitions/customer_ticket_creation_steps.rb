9666c4c8d6ce7494856c23cd8e3bcb507f309fd8
Alexei Volkov <lifecoder.ua@gmail.com>
2015-05-03 16:40:53 +0300

    cucumber ticket creation tests

--- features/step_definitions/customer_ticket_creation_steps.rb
+++ features/step_definitions/customer_ticket_creation_steps.rb
@@ -1,11 +1,54 @@
-Given(/^there's a tiket named "([^"]*)" exists$/) do |arg|
-  FactoryGirl.create(:ticket, subject: arg)
+Given(/^there's a ticket named "([^"]*)" exists$/) do |subject|
+  FactoryGirl.create(:ticket, subject: subject)
 end
 
-When(/^I am on the homepage$/) do
+Given(/^I am on the homepage$/) do
   visit root_path
 end
 
-Then(/^I should see the "(.*?)" title/) do |title|
+Given(/^I fill "([^"]*)" ticket with email "([^"]*)", name "([^"]*)" and body "([^"]*)"$/) do |title, email, name, body|
+  step %{I fill in "Subject" with "#{title}"}
+  step %{I fill in "Email" with "#{email}"}
+  step %{I fill in "Name" with "#{name}"}
+  step %{I fill in "Body" with "#{body}"}
+end
+
+Given(/^I create a "([^"]*)" ticket with email "([^"]*)", name "([^"]*)" and body "([^"]*)"$/) do |title, email, name, body|
+  step %{I fill "#{title}" ticket with email "#{email}", name "#{name}" and body "#{body}"}
+  step %{I press "Create"}
+  step %{I should see the "Server is down" title}
+  step %{I should see the "Waiting for Staff Response" status}
+end
+
+Given(/^I can't create a ticket without email$/) do
+  title = 'Some title'
+  email = ''
+  name = 'The Name'
+  body = 'The Ticket Body'
+  step %{I fill "#{title}" ticket with email "#{email}", name "#{name}" and body "#{body}"}
+  step %{I press "Create"}
+  step %{I should see the inline error "can't be blank" for "Email"}
+end
+
+
+When(/^I fill in "([^"]*)" with "([^"]*)"$/) do |field, value|
+  fill_in field, with: value
+end
+
+When(/^I press "([^"]*)"/) do |button|
+  find_button(button).click
+end
+
+
+
+Then(/^I should see the "(.*?)" title$/) do |title|
   expect(page).to have_content(title)
+end
+
+Then(/^I should see the "(.*?)" status$/) do |status|
+  expect(page).to have_content("Status: #{status}")
+end
+
+Then(/^I should see the inline error "([^"]*)" for "([^"]*)"$/) do |message, field|
+  expect(find(".#{field.downcase} .error").text).to eq(message)
 end
--- features/ticket_create.feature
+++ features/ticket_create.feature
@@ -1,6 +1,14 @@
 Feature: Customer creates a ticket
 
+  Background:
+    Given there's a ticket named "My cool ticket" exists
+    And I am on the homepage
+
   Scenario: Viewing home page
-    Given there's a tiket named "My cool ticket" exists
-    When I am on the homepage
     Then I should see the "New ticket" title
+
+  Scenario: I create a valid ticket
+    Given I create a "Server is down" ticket with email "test@example.com", name "That One Customer" and body "Everything is broken!"
+
+  Scenario: I forgot to enter Email
+    Given I can't create a ticket without email
